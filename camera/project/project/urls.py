b9d20db5969178d22a35ee954e1ca41c3df75d6b
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2019-07-11 19:59:44 +0900

    静的ファイルのURLパスを追加

--- camera/project/project/urls.py
+++ camera/project/project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpaterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('camera/', include(*'app.urls')),
 ]
+
+urlpatterns += staticfiles_urlpaterns()
+    
