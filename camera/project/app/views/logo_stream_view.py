052bf0f6676783233bc857bf064d11f063bab19c
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2019-07-14 21:07:57 +0900

    画像の位置を修正

--- camera/project/app/views/logo_stream_view.py
+++ camera/project/app/views/logo_stream_view.py
@@ -11,12 +11,37 @@ PATH = os.path.dirname(__file__)
 
 def overlay_logo(frame):
     img = cv2.imread(PATH+'/rancher.png')
-    # img = cv2.imread("rancher.png", -1)
 
     # 画像のサイズを取得する
-    rows, cols, _ = img.shape
-    roi = frame[0:rows, 0:cols]
-    img = img[0:rows, 0:cols]
+    # img_widthは画像の横幅, img_heightは画像の縦幅
+    img_height, img_width, _ = img.shape[:3]
+    # print(img_width)
+    # img_height, img_width = img.shape[:2]
+    img = img[0:img_height, 0:img_width]
+
+    # フレームサイズの取得
+    frame_width = len(frame[0])
+    print(frame_width)
+    frame_height = len(frame)
+    # フレームと画像サイズの差を取得
+    delta_width = frame_width - img_width
+    # print("delta_width:" + str(delta_width))
+    # start_xは画像の配置先となるフレーム左上端のx座標
+    start_x = int(delta_width / 2)
+    # start_yは画像の配置先となるフレーム左上端のy座標
+    start_y = frame_height - img_height
+
+    # end_xは画像の配置先となるフレーム右下端のx座標
+    end_x = start_x + img_width
+    # end_yは画像の配置先となるフレーム右下端のy座標
+    end_y = frame_height
+
+    # 重ね合わせ用にフレームから切り取る範囲を確認
+    ## roi = frame[0:img_height, 0:img_width]
+    # print(str(start_x) + "," + str(start_y) + " / " + str(end_x) + "," + str(end_y))
+    roi = frame[ start_y:end_y, start_x:end_x ]
+
+
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, mask = cv2.threshold(img_gray, 10, 255, cv2.THRESH_BINARY)
@@ -25,8 +50,11 @@ def overlay_logo(frame):
     bg = cv2.bitwise_and(roi,roi,mask=mask_inv)
     fg = cv2.bitwise_and(img,img,mask=mask)
 
+
     dst = cv2.add(bg, fg)
-    frame[0:rows, 0:cols] = dst
+    # frame[start_x:end_x, start_y:frame_height] = dst
+    frame[start_y:frame_height, start_x:end_x] = dst
+
 
     # return frame
     return frame
