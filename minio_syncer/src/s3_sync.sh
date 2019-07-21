f36456fbda4f6c16e967e801b08a56b2ccc797ad
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2019-07-21 15:34:50 +0900

    minio上の画像との同期用のファイルを準備

---
+++ minio_syncer/src/s3_sync.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# ENDPOINT_URL='http://pi03.web.ryoma0923.work:9000'
+# BUCKET_NAME='showks'
+# export AWS_ACCESS_KEY_ID='miniokey'
+# export AWS_SECRET_ACCESS_KEY='miniostorage'
+
+echo "ENDPOINT URL:" $ENDPOINT_URL
+echo "BUCKET NAME:" $BUCKET_NAME
+
+aws --endpoint-url $ENDPOINT_URL s3 sync s3://$BUCKET_NAME ./images
